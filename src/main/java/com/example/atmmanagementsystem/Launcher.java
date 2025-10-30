6a1aefc1e7922454878950eabe1a89ed645d5419
Samiul Hasan <samiulxhasan650@gmail.com>
2025-10-30 22:07:33 +0600

    Project Initialize

---
+++ src/main/java/com/example/atmmanagementsystem/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.atmmanagementsystem;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
